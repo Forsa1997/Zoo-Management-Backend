7dccf40cada03e4f0fac07f381a7ffd8aa7d8758
Netrax23 <nico.masur@gmx.de>
2022-05-05 14:59:46 +0200

    init commit

---
+++ src/main/java/com/example/zoomanagementbackend/ZooManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoomanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zoomanagementbackend/ZooManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoomanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
