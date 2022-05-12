2b0c14eb4433615b4d257a6661d4faaa3899c2c7
Nitschke, Daniel <nitschke.daniel@volkswagen.de>
2022-05-13 00:00:27 +0200

    Application cleanup and DatabasePopulationRunner addition to add dummy data to the deployment

--- src/main/java/de/volkswagen/Application.java
+++ src/main/java/de/volkswagen/Application.java
@@ -1,46 +1,12 @@
 package de.volkswagen;
 
-import de.volkswagen.animal.AnimalType;
-import de.volkswagen.enclosure.Enclosure;
-import de.volkswagen.enclosure.EnclosureService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import java.util.Arrays;
-import java.util.List;
-
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	// @Profile({"prod", "production"}) // TODO enable only during production
-	CommandLineRunner run(
-		EnclosureService enclosureService
-	) {
-		return args -> {
-			enclosureService.create(Enclosure.builder()
-					.name("Affenhaus")
-					.description("Kleine Äffchen")
-					.animalTypes(Arrays.asList(AnimalType.MONKEY))
-					.build());
-			enclosureService.create(Enclosure.builder()
-					.name("Schlangenkuhle")
-					.description("Nicht anfassen")
-					.animalTypes(Arrays.asList(AnimalType.SNAKE))
-					.build());
-			enclosureService.create(Enclosure.builder()
-					.name("Savannah")
-					.description("sehr warm")
-					.animalTypes(Arrays.asList(AnimalType.GIRAFFE, AnimalType.HYENA, AnimalType.ZEBRA))
-					.build());
-		};
 	}
 }
---
+++ src/main/java/de/volkswagen/DatabasePopulationRunner.java
@@ -0,0 +1,73 @@
+package de.volkswagen;
+
+import de.volkswagen.animal.Animal;
+import de.volkswagen.animal.AnimalService;
+import de.volkswagen.animal.AnimalType;
+import de.volkswagen.enclosure.Enclosure;
+import de.volkswagen.enclosure.EnclosureService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Profile("!test")
+@Component
+public class DatabasePopulationRunner implements CommandLineRunner {
+
+    private EnclosureService enclosureService;
+    private AnimalService animalService;
+
+    public DatabasePopulationRunner(
+            EnclosureService enclosureService,
+            AnimalService animalService
+    ) {
+        this.enclosureService = enclosureService;
+        this.animalService = animalService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        enclosureService.create(Enclosure.builder()
+                .name("Affenhaus")
+                .description("Kleine Äffchen")
+                .animalTypes(Collections.singletonList(AnimalType.MONKEY))
+                .animals(Arrays.asList(
+                        animalService.create(Animal.builder()
+                                .name("Hugo")
+                                .animalType(AnimalType.MONKEY)
+                                .build()),
+                        animalService.create(Animal.builder().name("Uschi").animalType(AnimalType.MONKEY).build()),
+                        animalService.create(Animal.builder().name("Franko").animalType(AnimalType.MONKEY).build())
+                ))
+                .build());
+
+        enclosureService.create(Enclosure.builder()
+                .name("Schlangenkuhle")
+                .description("Nicht anfassen")
+                .animalTypes(Collections.singletonList(AnimalType.SNAKE))
+                .animals(Arrays.asList(
+                        animalService.create(Animal.builder().name("Snekmann").animalType(AnimalType.SNAKE).build()),
+                        animalService.create(Animal.builder().name("Snekfrau").animalType(AnimalType.SNAKE).build()),
+                        animalService.create(Animal.builder().name("Snekperson").animalType(AnimalType.SNAKE).build()),
+                        animalService.create(Animal.builder().name("Sneksnek").animalType(AnimalType.SNAKE).build()),
+                        animalService.create(Animal.builder().name("Snekhelikopter").animalType(AnimalType.SNAKE).build())
+                ))
+                .build());
+
+        enclosureService.create(Enclosure.builder()
+                .name("Savannah")
+                .description("sehr warm")
+                .animalTypes(Arrays.asList(AnimalType.GIRAFFE, AnimalType.HYENA, AnimalType.ZEBRA))
+                .animals(Arrays.asList(
+                        animalService.create(Animal.builder().name("Nico").animalType(AnimalType.GIRAFFE).build()),
+                        animalService.create(Animal.builder().name("Odin").animalType(AnimalType.GIRAFFE).build()),
+                        animalService.create(Animal.builder().name("Zeus").animalType(AnimalType.HYENA).build()),
+                        animalService.create(Animal.builder().name("Kaan").animalType(AnimalType.ZEBRA).build()),
+                        animalService.create(Animal.builder().name("Emre").animalType(AnimalType.ZEBRA).build())
+                ))
+                .build());
+    }
+}
