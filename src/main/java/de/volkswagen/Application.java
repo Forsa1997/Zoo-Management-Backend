591d89f0dcec2c35e9c91f74a3b3ca4c2de8d6be
Nitschke, Daniel <nitschke.daniel@volkswagen.de>
2022-05-12 13:57:29 +0200

    yet another CORS fix

--- src/main/java/de/volkswagen/Application.java
+++ src/main/java/de/volkswagen/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,7 @@ public class Application {
 	}
 
 	@Bean
+	// @Profile({"prod", "production"}) // TODO enable only during production
 	CommandLineRunner run(
 		EnclosureService enclosureService
 	) {
